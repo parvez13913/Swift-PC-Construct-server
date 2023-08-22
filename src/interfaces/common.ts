a8aa54ae40e5b125c2b04e9d9138769880be473c
parvez13913 <parvezz13913@gmail.com>
2023-08-22 13:49:24 +0600

    [DONE] handel zod error and Api Error

---
+++ src/interfaces/common.ts
@@ -0,0 +1,17 @@
+import { IGenericErrorMessage } from './error';
+
+export type IGenericErrorRespons = {
+  statusCode: number;
+  message: string;
+  errorMessages: IGenericErrorMessage[];
+};
+
+export type IGenericResponse<T> = {
+  meta: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+
+  data: T;
+};
