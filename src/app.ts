4dbc082db89ed64f2aa7f59860937be0288aad26
parvez13913 <parvezz13913@gmail.com>
2023-08-22 13:58:53 +0600

    [DONE] globalErrorHandler.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import cors from "cors";
-import express, { Application, NextFunction, Request, Response } from "express";
-// import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import cors from 'cors';
+import express, { Application } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
 const app: Application = express();
 
@@ -13,21 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 // app.use("/api/v1/");
 
 // global Error Handler
-// app.use(globalErrorHandler);
-
-// Handle NotFound Route
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//   res.status(httpStatus.NOT_FOUND).json({
-//     success: false,
-//     message: "Not Found",
-//     errorMessages: [
-//       {
-//         path: req.originalUrl,
-//         message: "API Not Found",
-//       },
-//     ],
-//   });
-//   next();
-// });
+app.use(globalErrorHandler);
 
 export default app;
