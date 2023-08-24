cabc17edbb24dfb088726c08114f9c5720210447
parvez13913 <parvezz13913@gmail.com>
2023-08-24 20:26:59 +0600

    [DONE] processor interface

---
+++ src/app/modules/processor/processor.interface.ts
@@ -0,0 +1,12 @@
+import { Model } from 'mongoose';
+
+export type IProcessor = {
+  processor: string;
+  category: string;
+  image: string;
+  price: string;
+  status: 'In Stock' | 'Out of stock';
+  rating: string;
+};
+
+export type ProcessorModel = Model<IProcessor, Record<string, unknown>>;
